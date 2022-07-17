7b5b92c6168a18c9d79f2d173387ce30b34906c9
egorm <emasorin@rvision.ru>
2022-07-17 20:18:45 +0300

    убрал hold browser

--- src/test/java/tests/DemoWithTestData.java
+++ src/test/java/tests/DemoWithTestData.java
@@ -29,7 +29,6 @@ public class DemoWithTestData {
     static void setUp() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.holdBrowserOpen = true;
     }
 
 
